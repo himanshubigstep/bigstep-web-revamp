4649274fa280b7402be80b7993a38a0fb69459ef
Himanshu Singh <himanshu.singh@bigsteptech.com>
2024-12-18 19:49:21 +0530

    changes-feedback-footer

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -6,23 +6,23 @@ import Image from 'next/image'
 import FooterNavigation from './footer-nav/FooterNavigation'
 import FooterBottom from './footer-bottom/FooterBottom'
 import { fetchFooterData, subscriberFormData } from '@/api-data/api'
-import { toast } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css';
-import InputField from '../common/input-fields/InputField'
+// import { toast } from 'react-toastify'
+// import 'react-toastify/dist/ReactToastify.css';
+// import InputField from '../common/input-fields/InputField'
 import LoaderSpinner from '../common/loader-spinner/LoadingSpinner'
 
 const Footer = () => {
   const [footerData, setFooterData] = useState<any>(null);
-  const [inputValue, setInputValue] = useState({
-    name: '',
-    email: ''
-  });
-  const [isSubmitting, setIsSubmitting] = useState(false);
-  const [submitError, setSubmitError] = useState<string | null>(null);
-  const [formErrors, setFormErrors] = useState({
-    name: '',
-    email: '',
-  });
+  // const [inputValue, setInputValue] = useState({
+  //   name: '',
+  //   email: ''
+  // });
+  // const [isSubmitting, setIsSubmitting] = useState(false);
+  // const [submitError, setSubmitError] = useState<string | null>(null);
+  // const [formErrors, setFormErrors] = useState({
+  //   name: '',
+  //   email: '',
+  // });
 
   useEffect(() => {
     const fetchFooterDataResponse = async () => {
@@ -37,108 +37,108 @@ const Footer = () => {
     fetchFooterDataResponse();
   }, []);
 
-  const handleInputChange = (field: 'name' | 'email') => (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    setInputValue(prevState => ({
-      ...prevState,
-      [field]: event.target.value
-    }));
-  };
+  // const handleInputChange = (field: 'name' | 'email') => (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  //   setInputValue(prevState => ({
+  //     ...prevState,
+  //     [field]: event.target.value
+  //   }));
+  // };
 
-  const validateForm = () => {
-    const errors = { name: '', email: '' };
-    let isValid = true;
+  // const validateForm = () => {
+  //   const errors = { name: '', email: '' };
+  //   let isValid = true;
 
-    if (!inputValue.name.trim()) {
-      errors.name = 'Name is required';
-      isValid = false;
-    }
+  //   if (!inputValue.name.trim()) {
+  //     errors.name = 'Name is required';
+  //     isValid = false;
+  //   }
 
-    if (!inputValue.email.trim()) {
-      errors.email = 'Email is required';
-      isValid = false;
-    } else {
-      // Regex to validate email format
-      const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-      if (!emailRegex.test(inputValue.email)) {
-        errors.email = 'Email is invalid';
-        isValid = false;
-      } else {
-        // Additional check for multiple domain extensions
-        const domainPart = inputValue.email.split('@')[1];
-        const domainRegex = /^[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-        if (domainPart.split('.').length > 2 || !domainRegex.test(domainPart)) {
-          errors.email = 'Invalid email format. Multiple domain extensions are not allowed.';
-          isValid = false;
-        }
-      }
-    }
+  //   if (!inputValue.email.trim()) {
+  //     errors.email = 'Email is required';
+  //     isValid = false;
+  //   } else {
+  //     // Regex to validate email format
+  //     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  //     if (!emailRegex.test(inputValue.email)) {
+  //       errors.email = 'Email is invalid';
+  //       isValid = false;
+  //     } else {
+  //       // Additional check for multiple domain extensions
+  //       const domainPart = inputValue.email.split('@')[1];
+  //       const domainRegex = /^[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  //       if (domainPart.split('.').length > 2 || !domainRegex.test(domainPart)) {
+  //         errors.email = 'Invalid email format. Multiple domain extensions are not allowed.';
+  //         isValid = false;
+  //       }
+  //     }
+  //   }
 
-    setFormErrors(errors);
-    return isValid;
-  };
+  //   setFormErrors(errors);
+  //   return isValid;
+  // };
 
-  const handelSubscription = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    setIsSubmitting(true);
-    setSubmitError(null);
+  // const handelSubscription = async (event: React.FormEvent<HTMLFormElement>) => {
+  //   event.preventDefault();
+  //   setIsSubmitting(true);
+  //   setSubmitError(null);
 
-    if (!validateForm()) {
-      setIsSubmitting(false);
-      return;
-    }
+  //   if (!validateForm()) {
+  //     setIsSubmitting(false);
+  //     return;
+  //   }
 
-    try {
-      const response = await subscriberFormData(inputValue);
-      if (response) {
-        console.log('Form submitted successfully:', response);
-        setInputValue({ name: '', email: '' });
-        toast.success('Form has been submitted successfully!', {
-          autoClose: 5000,
-        });
-      } else {
-        const errorResponse = await fetch(
-          `${process.env.NEXT_PUBLIC_BASE_URL}/subscribers`,
-          {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ data: inputValue }),
-          }
-        ).then(res => res.json());
+  //   try {
+  //     const response = await subscriberFormData(inputValue);
+  //     if (response) {
+  //       console.log('Form submitted successfully:', response);
+  //       setInputValue({ name: '', email: '' });
+  //       toast.success('Form has been submitted successfully!', {
+  //         autoClose: 5000,
+  //       });
+  //     } else {
+  //       const errorResponse = await fetch(
+  //         `${process.env.NEXT_PUBLIC_BASE_URL}/subscribers`,
+  //         {
+  //           method: 'POST',
+  //           headers: {
+  //             'Content-Type': 'application/json',
+  //           },
+  //           body: JSON.stringify({ data: inputValue }),
+  //         }
+  //       ).then(res => res.json());
 
-        if (errorResponse?.error?.details?.errors) {
-          errorResponse.error.details.errors.forEach((error: { path: any[]; message: string; }) => {
-            const path = error?.path?.join(', ') || 'Unknown Field';
-            const message = error?.message || 'An error occurred';
-            toast.error(`${path}: ${message}`, {
-              autoClose: 5000,
-            });
-          });
-        } else {
-          toast.error(`${errorResponse?.error?.message} || 'Failed to submit the form. Please try again.'`, {
-            autoClose: 5000,
-          });
-        }
-      }
-    } catch (error: any) {
-      console.error('Error submitting the form:', error);
+  //       if (errorResponse?.error?.details?.errors) {
+  //         errorResponse.error.details.errors.forEach((error: { path: any[]; message: string; }) => {
+  //           const path = error?.path?.join(', ') || 'Unknown Field';
+  //           const message = error?.message || 'An error occurred';
+  //           toast.error(`${path}: ${message}`, {
+  //             autoClose: 5000,
+  //           });
+  //         });
+  //       } else {
+  //         toast.error(`${errorResponse?.error?.message} || 'Failed to submit the form. Please try again.'`, {
+  //           autoClose: 5000,
+  //         });
+  //       }
+  //     }
+  //   } catch (error: any) {
+  //     console.error('Error submitting the form:', error);
 
-      if (error.message === 'This attribute must be unique') {
-        setSubmitError('Email should be unique');
-      } else {
-        setSubmitError('An error occurred. Please try again.');
-      }
-    } finally {
-      setIsSubmitting(false);
-    }
-  };
+  //     if (error.message === 'This attribute must be unique') {
+  //       setSubmitError('Email should be unique');
+  //     } else {
+  //       setSubmitError('An error occurred. Please try again.');
+  //     }
+  //   } finally {
+  //     setIsSubmitting(false);
+  //   }
+  // };
 
-  useEffect(() => {
-    return () => {
-      toast.dismiss();
-    };
-  }, []);
+  // useEffect(() => {
+  //   return () => {
+  //     toast.dismiss();
+  //   };
+  // }, []);
 
   if (!footerData) {
     return <LoaderSpinner />;  // Avoid returning `null`, show a loading spinner instead
@@ -157,7 +157,7 @@ const Footer = () => {
           alt='Footer Background'
           className='absolute left-0 right-0 top-0 bottom-0 w-full h-full object-contain bg-repeat-x opacity-80'
         />
-        <div className='relative mb-8 w-full mx-auto border-b-[1px] pb-8'>
+        {/* <div className='relative mb-8 w-full mx-auto border-b-[1px] pb-8'>
           <form onSubmit={handelSubscription} className='grid lg:grid-cols-3 md:grid-cols-3 gap-4 lg:max-w-[75%] md:max-w-[75%] mx-auto'>
             <InputField
               type='text'
@@ -188,7 +188,7 @@ const Footer = () => {
             </button>
             {submitError && <p className='text-red-500 text-left absolute left-0 top-32'>{submitError}</p>}
           </form>
-        </div>
+        </div> */}
         <div className='relative w-full h-full max-w-[1440px] mx-auto flex flex-col lg:flex-row justify-between'>
           <div className='h-full lg:w-1/4 w-full flex flex-col'>
             <Image
